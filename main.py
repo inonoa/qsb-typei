e7fc40c888fed489e271a6ecd3fa4802be5a0fe3
inonoa <himura.kennsinn.degozaru@gmail.com>
2019-06-05 21:58:02 +0900

    peekをむりゃり実装

--- main.py
+++ main.py
@@ -4,12 +4,30 @@ import time
 import sys
 import os
 
-class QueueWithName:
+class IQ:
     def __init__(self, name):
         self.name = name
         self.q = qu.Queue()
+        self.top = None
+    def enq(self,put):
+        if self.top==None:
+           self.top = put
+        else:
+            self.q.put(put)
+    def deq(self,):
+        if self.top==None:
+            say("キューが空です。")
+        else:
+            whatwastop = self.top
+            self.top = self.q.get()
+            say(whatwastop+"を出しました。")
+    def peek(self,):
+        if self.top==None:
+            return ":null:"
+        else:
+            return self.top
 
-qs = [QueueWithName("きゅー")]
+qs = [IQ("きゅー")]
 hajimete = True
 
 boku = SlackClient(os.environ["SLACK_API_TOKEN"])
@@ -42,47 +60,51 @@ while True:
 
                         # Dequeue(Queueから一つ取り出す)
                         if rtm["text"][:4] == "deq " and len(rtm["text"])>4:
-                            for qwn in qs:
-                                if qwn.name==rtm["text"][4:]:
-                                    if qwn.q.empty():
-                                        say("キューが空です。")
-                                    else:
-                                        say(qwn.q.get()+"を出しました。")
+                            for iq in qs:
+                                if iq.name==rtm["text"][4:]:
+                                    iq.deq()
                                     break
                             else:
                                 say("そんな名前のキューは無いが？反論がないなら私の勝ちだが？")
                         
-                        # peek(Queueの先頭を見る)を実装しようとしたけどそんな関数はなかった？？
+                        # peek(Queueの先頭を見る)を実装しようとしたけどそんな関数はなかった？？→オブジェクト指向は便利(ほんま？)
+                        if rtm["text"][:5] == "peek " and len(rtm["text"])>5:
+                            for iq in qs:
+                                if iq.name==rtm["text"][5:]:
+                                    say("先頭は"+iq.peek()+"です。")
+                                    break
+                            else:
+                                say("そんな名前のキューは無いが？反論がないなら私の勝ちだが？")
 
                         # Enqueue(Queueに一つ入れる)
                         # これだけ複数入れられる罠(？)がある
                         elif rtm["text"][:4] == "enq " and len(rtm["text"])>4:
                             words = rtm["text"].split()
-                            for qwn in qs:
-                                if qwn.name==words[1]:
+                            for iq in qs:
+                                if iq.name==words[1]:
                                     for w in words[2:]:
                                         say(w+"を入れます。")
-                                        qwn.q.put(w)
+                                        iq.enq(w)
                                     break
                             else:
                                 say("そんな名前のキューは無いが？反論がないなら私の勝ちだが？")
 
                         # new Queue();(キューを追加)
                         elif rtm["text"][:9] == "create q " and len(rtm["text"])>9:
-                            for qwn in qs:
-                                if qwn.name==rtm["text"][9:]:
+                            for iq in qs:
+                                if iq.name==rtm["text"][9:]:
                                     say("既に同名のキューが存在します。")
                                     break
                             else:
-                                qs.append(QueueWithName(rtm["text"][9:]))
+                                qs.append(IQ(rtm["text"][9:]))
                                 say("キュー "+rtm["text"][9:]+" を追加しました。")
 
                         # Remove(Queueを削除)
                         elif (rtm["text"][:9] == "remove q " or rtm["text"][:9] == "delete q ") and len(rtm["text"])>9:
-                            for qwn in qs:
-                                if qwn.name==rtm["text"][9:]:
+                            for iq in qs:
+                                if iq.name==rtm["text"][9:]:
                                     say("キュー "+rtm["text"][9:]+" を削除します。")
-                                    qs.remove(qwn)
+                                    qs.remove(iq)
                                     break
                             else:
                                 say("そんな名前のキューは無いが？反論がないなら私の勝ちだが？")
@@ -94,12 +116,18 @@ while True:
                             if(len(qs))==0:
                                 say(":iie:")
                             else:
-                                for qwn in qs:
-                                    s += qwn.name + "\n"
+                                for iq in qs:
+                                    s += iq.name + "\n"
                                 say(s)
+                        
+                        # kill qsb (殺す)
+                        elif rtm["text"] == "kill qsb":
+                            say("この後亡くなったんだよね…")
+                            sys.exit()
 
                 # イベント取得間隔は三秒(以上)
                 time.sleep(3)
+                print(3)
 
         # 任意の例外で強制終了のメッセージだして(出せない時もあるだろうけど)終了
         except KeyboardInterrupt:
